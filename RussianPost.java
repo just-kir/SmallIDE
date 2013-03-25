a1d1d2389ba03ab680937c5001e804d5b5e670a9
Kirill Pavlenko <dontwritehim@gmail.com>
2013-03-25 13:56:39 +0400

    RP starting

---
+++ RussianPost.java
@@ -0,0 +1,23 @@
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Set;
+
+
+
+public class RussianPost {
+	
+	Hashtable IncPost = new Hashtable();
+	
+	
+	public void AddIncPostBox (String name) {
+	
+	}
+	
+	public void AddOutPostBox (String name) {
+	
+	} 
+
+
+
+}
