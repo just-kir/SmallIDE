a94c9adffeaab680c020c178141e548335f146e2
Kirill Pavlenko <dontwritehim@gmail.com>
2013-03-25 17:54:07 +0400

    RussianPost v0.1

--- RussianPost.java
+++ RussianPost.java
@@ -1,25 +1,27 @@
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Hashtable;
-import java.util.Set;
-
+import java.util.*;
 
 
 public class RussianPost {
 	
 	Hashtable<String, MsgRcvr> IncPost = new Hashtable<String, MsgRcvr>();
 	
+	Hashtable<String, ArrayList<MsgRcvr>> OutPost= new Hashtable<String, ArrayList<MsgRcvr>>(); // fuck!! It works!
+	
 	
 	public void addIncPostBox (String name, MsgRcvr mr) {
-		IncPost.put(name, mr);// smth wrong
+		IncPost.put(name, mr);
 	}
 	
-	public void addOutPostBox(String name) {
-	
+	public OutBox addOutPostBox(String name) {
+		ArrayList<MsgRcvr> sub = new ArrayList<MsgRcvr>();
+		OutPost.put(name, sub); // adding note in OutPost 
+		
+		OutBox newbox = new OutBox(name);
+		return newbox;
 	} 
 	
-	public void subscribe(String name) {
-		
+	public void subscribe(String name, MsgRcvr self) {
+		OutPost.get(name).add(self);
 	}
 	
 	
