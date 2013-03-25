688f854209cf8fd0ea0e8703327db33728b53655
Kirill Pavlenko <dontwritehim@gmail.com>
2013-03-25 17:53:01 +0400

    OutBox v0.1

--- OutBox.java
+++ OutBox.java
@@ -1,3 +1,20 @@
-public class OutBox {
+import java.util.*;
 
+public class OutBox {
+	
+	ArrayList<MsgRcvr> list;
+	
+	
+	public OutBox(ArrayList<MsgRcvr> sub) {
+		list = sub;
+	}
+	
+	
+	public void putAndSend(String msg) {
+		// sending messages to subscribers
+		for(int i = 0; i< list.size(); i++) {
+			list.get(i).handle(msg);
+		}
+		
+	}
 }
