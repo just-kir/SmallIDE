c104d2134590f16dadf9dadd5aaac38e77136638
Kirill Pavlenko <dontwritehim@gmail.com>
2013-04-12 18:52:35 +0400

    modified add() and constructor in Configuration

--- ConfTestModule.java
+++ ConfTestModule.java
@@ -3,7 +3,7 @@
 public class ConfTestModule extends Module {
 	public ConfTestModule(MainFrame mf, RussianPost rp, Configuration conf){
 		super(mf, rp, conf);
-
+		
 	
 	}
 }
--- Configuration.java
+++ Configuration.java
@@ -4,13 +4,29 @@ import java.util.*;
 public class Configuration implements Serializable{
 
 	Hashtable<Module, ArrayList<Object>> SysConf = new Hashtable<Module, ArrayList<Object>>();
-
+	
+	public Configuration() {
+		// trying to fill SysConf
+		try {
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream("SysConf.ser"));
+			SysConf =(Hashtable) is.readObject(); // makes some unsefty things
+		} catch(Exception ex) {
+			// exception
+		}
+	}
 
 	public void add(Module self, Object forserialize) {
 		SysConf.get(self).add(forserialize); // be carefull!
+		try {
+			ObjectOutputStream os = new ObjectOutputStream (new FileOutputStream("SysConf.ser")); // not sure about it
+			os.writeObject(SysConf); // writing object
+			os.close();
+		} catch(IOException ex) {
+			// exception!
+		}
 	}
 	
-	public Object get() {
+	public Object get() { // not working yet
 		Object c = null;
 		return c;
 	}
