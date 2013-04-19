966f1962c477e4571b1be696cf002eb464254cc5
Kirill Pavlenko <dontwritehim@gmail.com>
2013-04-19 17:09:49 +0400

    Non-working conf

--- Configuration.java
+++ Configuration.java
@@ -3,45 +3,55 @@ import java.util.*;
 
 public class Configuration implements Serializable{
 
-	Hashtable<Module, ArrayList<Object>> SysConf = new Hashtable<Module, ArrayList<Object>>();
+	private Hashtable<Module, Hashtable<String, Object>> sysConf = new Hashtable<Module, Hashtable<String, Object>>();
 	
 	public Configuration() {
 		// trying to fill SysConf
 		try {
 			ObjectInputStream is = new ObjectInputStream(new FileInputStream("SysConf.ser"));
-			SysConf =(Hashtable) is.readObject(); // makes some unsefty things
+			sysConf =(Hashtable) is.readObject(); // makes some unsefty things
 		} catch(Exception ex) {
 			// exception
 		}
 	}
 
 	public void add(Module self, Object forserialize) {
-		SysConf.get(self).add(forserialize); // be carefull!
+		// add variable for serialization and serializing SysConf
+		//SysConf.get(self).add(forserialize); // be carefull!
 		try {
 			ObjectOutputStream os = new ObjectOutputStream (new FileOutputStream("SysConf.ser")); // not sure about it
-			os.writeObject(SysConf); // writing object
+			os.writeObject(sysConf); // writing object
 			os.close();
 		} catch(IOException ex) {
 			// exception!
 		}
 	}
 	
-	public Object get() { // not working yet
+	public Object get(Module name, Object objname) { // not working yet
 		Object c = null;
+		// Restore and give away	
+		try {
+			ObjectInputStream is = new ObjectInputStream(new FileInputStream("SysConf.ser"));
+			sysConf =(Hashtable) is.readObject(); // makes some unsefty things
+		} catch(Exception ex) {
+			// exception
+		}
+		
+		
 		return c;
 	}
 	
 	public boolean ask(Module self) {
 		boolean c = false;
-		if (SysConf.get(self) != null) {
+		if (sysConf.get(self) != null) {
 			c = true;
 		}
 		return c;
 	}
 	
 	public void register(Module self){
-		ArrayList<Object> myobjects = new ArrayList<Object>();
-		SysConf.put(self, myobjects); // register module 
+		Hashtable<String, Object> myobjects = new Hashtable<String, Object>();
+		sysConf.put(self, myobjects); // register module 
 	
 	}
 }
